15df1adbc0fe03daf58567b7eca59bdc39c66012
Nikita Tomchik <cdayz@yandex.ru>
2022-05-12 20:09:54 +0300

    Add use_prometheus_metrics function, to automaticaly enable metrics

--- blacksheep_prometheus/__init__.py
+++ blacksheep_prometheus/__init__.py
@@ -1,7 +1,27 @@
+from typing import Optional
+
+from blacksheep.server import Application
+
 from .middleware import PrometheusMiddleware
 from .view import metrics
 
+
+def use_prometheus_metrics(
+    app: Application,
+    *,
+    endpoint: str = "/metrics/",
+    middleware: Optional[PrometheusMiddleware] = None,
+) -> None:
+    """
+    Configures the given application to use Prometheus and provide services that can be
+    injected in request handlers.
+    """
+    middleware = middleware or PrometheusMiddleware()
+    app.middlewares.append(middleware)
+    app.router.add_get(endpoint, metrics)
+
+
 __all__ = [
-    'metrics',
+    'use_prometheus_metrics',
     'PrometheusMiddleware',
 ]
